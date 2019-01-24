c435ddb5b3427d4fc401322446f3e2436e1723c9
Marco Di Bella <dibella@rechtsdepesche.de>
2019-01-24 11:03:40 +0100

    einzelansicht der referenten

--- mdb-congressomat/single-speaker.php
+++ mdb-congressomat/single-speaker.php
@@ -8,18 +8,34 @@
 ?>
 <?php get_header(); ?>
 <main id="main">
+<section class="module">
+<div class="module-wrapper">
+<div class="module-content">
 <?php
 while( have_posts() ) :
     the_post();
-    //the_content();
+
+    // Referentendaten holen
     $speaker = mdb_get_speaker_info( get_the_ID() );
+?>
+<article class="speaker-profile">
+<div><?php echo get_the_post_thumbnail( $speaker[ 'id' ], 'full' ); ?></div>
+<div>
+<h2 class="speaker-title-name"><?php echo $speaker[ 'title_name' ]; ?></h2>
+<?php
+    // Position oder Berufstitel bekannt?
+    if( !empty( $speaker[ 'position' ] ) ) :
+?>
+<p class="speaker-position"><?php echo $speaker[ 'position' ]; ?></p>
+<?php
+endif;
 
-    echo get_the_post_thumbnail( $speaker[ 'id' ], 'full' );
-    echo $speaker[ 'title_name' ];
-    echo '<br>';
-    echo $speaker[ 'position' ];
-
+    // Links zu Soziale Medien vorhanden?
     if( have_rows( 'referent-social-media' ) ) :
+?>
+<div class="speaker-social-media">
+<ul>
+<?php
         while( have_rows( 'referent-social-media' ) ) :
             the_row();
 
@@ -34,7 +50,7 @@ while( have_posts() ) :
 
             $service = get_sub_field( 'referent-web-service' );
 
-            echo sprintf( '<a href="%1$s" target="_blank" title="%2$s">%3$s</a>',
+            echo sprintf( '<li><a href="%1$s" target="_blank" title="%2$s">%3$s</a></li>',
                           get_sub_field( 'referent-web-service-url' ),
                           sprintf( __( 'Profil von %1$s auf %2$s', TEXT_DOMAIN ),
                                    $speaker[ 'name' ],
@@ -42,9 +58,26 @@ while( have_posts() ) :
                                    $services[ $service ][ 'name' ]
                       /* icon-button */ );
         endwhile;
+?>
+</ul>
+</div>
+<?php
     endif;
-    echo $speaker[ 'description' ];
+
+    // Ausführliche Beschreibung vorhanden?
+    if( !empty( $speaker[ 'description' ] ) ) :
+?>
+<div class="speaker-description"><?php echo $speaker[ 'description' ]; ?></div>
+<?php
+    endif;
+?>
+</div>
+</article>
+<?php
 endwhile;
 ?>
+</div>
+</div>
+</section>
 </main>
 <?php get_footer(); ?>
