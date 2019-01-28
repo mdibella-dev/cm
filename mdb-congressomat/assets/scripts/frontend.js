1431d2c874c621378976f223673829a356238981
Marco Di Bella <dibella@rechtsdepesche.de>
2019-01-28 10:02:28 +0100

    Create frontend.js

---
+++ mdb-congressomat/assets/scripts/frontend.js
@@ -0,0 +1,21 @@
+jQuery(function($) {
+    /**
+     * MegaMenu
+     ***/
+
+/*    //duplicate hover state for parent menu when dropdown.
+    $( '#header-wrapper ul.dropdown' ).hover(
+        function() {
+    		$(this).parent().find('> a').addClass('expanded-menu');
+    	},
+        function(){
+    		$(this).parent().find('> a').removeClass('expanded-menu');
+    	});
+*/
+
+    $( '.menu-item-mega > a').click( function() {
+        //$(this).children( '.sub-menu' ).classList.toggle( 'open' );
+        $(this).parent().toggleClass('open' );
+    } );
+
+} );
