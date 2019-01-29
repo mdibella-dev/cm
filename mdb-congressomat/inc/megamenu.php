c30872d00d2a000fdfef2b58f1b27617ed22d9dd
Marco Di Bella <dibella@rechtsdepesche.de>
2019-01-29 10:15:44 +0100

    Update megamenu.php

--- mdb-congressomat/inc/megamenu.php
+++ mdb-congressomat/inc/megamenu.php
@@ -17,7 +17,7 @@
 
 function mdb_add_megamenu_metabox( $object )
 {
-	add_meta_box( 'megamenu-metabox', __( 'MegaMenu', TEXT_DOMAIN ), 'mdb_megamenu_metabox', 'nav-menus', 'side', 'default' );
+	add_meta_box( 'add-megamenu', __( 'MegaMenu', TEXT_DOMAIN ), 'mdb_megamenu_metabox', 'nav-menus', 'side', 'default' );
 
 	return $object;
 }
@@ -36,22 +36,22 @@ function mdb_megamenu_metabox()
 
     $_nav_menu_placeholder = 0 > $_nav_menu_placeholder ? $_nav_menu_placeholder - 1 : -1;
 ?>
-<div id="megamenudiv" class="customlinkdiv">
+<div id="megamenu" class="customlinkdiv">
 <input type="hidden" value="custom" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-type]" />
 
 <p id="menu-item-url-wrap" class="wp-clearfix">
 <label class="howto" for="custom-menu-item-url"><?php _e( 'URL' ); ?></label>
-<input id="custom-menu-item-url" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-url]" type="text" class="code menu-item-textbox" value="http://" />
+<input id="megamenu-item-url" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-url]" type="text" class="code menu-item-textbox" value="http://" />
 </p>
 
 <p id="menu-item-name-wrap" class="wp-clearfix">
 <label class="howto" for="custom-menu-item-name"><?php _e( 'Link Text' ); ?></label>
-<input id="custom-menu-item-name" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-title]" type="text" class="regular-text menu-item-textbox" />
+<input id="megamenu-item-name" name="menu-item[<?php echo $_nav_menu_placeholder; ?>][menu-item-title]" type="text" class="regular-text menu-item-textbox" />
 </p>
 
 <p class="button-controls wp-clearfix">
 <span class="add-to-menu">
-<input type="submit"<?php wp_nav_menu_disabled_check( $nav_menu_selected_id ); ?> class="button-secondary submit-add-to-menu right" value="<?php esc_attr_e('Add to Menu'); ?>" name="add-custom-menu-item" id="submit-customlinkdiv" />
+<input type="submit"<?php wp_nav_menu_disabled_check( $nav_menu_selected_id ); ?> class="button-secondary submit-add-to-menu right" value="<?php esc_attr_e('Add to Menu'); ?>" name="add-megamenu-item" id="submit-customlinkdiv" />
 <span class="spinner"></span>
 </span>
 </p>
