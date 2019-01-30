794c46a3a795b5a0a31bf5792d9fb9ec00f29278
Marco Di Bella <dibella@rechtsdepesche.de>
2019-01-30 14:53:07 +0100

    Create single.php

---
+++ mdb-congressomat/single.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Template für Beiträge
+ *
+ * @author  Marco Di Bella <mdb@marcodibella.de>
+ * @package mdb-congressomat
+ **/
+?>
+<?php get_header(); ?>
+<main id="main">
+<?php
+if ( have_posts() ) :
+    // Ausgabe puffern
+    ob_start();
+
+    while( have_posts() ) :
+        the_post();
+?>
+<article class="article">
+<?php
+        // Header
+        echo '<header>';
+        echo sprintf( '<h1>%1$s</h1>', get_the_title() );
+        echo '</header>';
+
+        // Inhalt
+        the_content();
+ ?>
+</article>
+<?php
+        // Ausgabenpuffer sichern; Pufferung beenden
+        $buffer = ob_get_contents();
+        ob_end_clean();
+
+        // Modul generieren
+        $args    = array(
+                   'class' => 'module-standard',
+                   'title' => ''
+                   );
+        echo mdb_get_module( $args, $buffer );
+    endwhile;
+endif;
+?>
+</main>
+<?php get_footer(); ?>
