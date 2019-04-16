74b6d837a4879aecc575c722166a56d42702387e
rdg-dibella <dibella@rechtsdepesche.de>
2019-04-16 08:00:14 +0200

    two co,umns

---
+++ mdb-congressomat/modulepage.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Template Name: Seite mit einem modularen Layout
+ *
+ * @author  Marco Di Bella <mdb@marcodibella.de>
+ * @package mdb-congressomat
+ * @uses    Plugin ACF
+ **/
+?>
+<?php get_header(); ?>
+<main id="main">
+<?php
+while( have_posts() ) :
+    the_post();
+
+    if( have_rows( 'module' ) ) :
+        while( have_rows( 'module' ) ) :
+            the_row();
+
+            switch( get_row_layout() ) :
+                case 'module-standard' :
+                    get_template_part( 'inc/modules/module-standard' );
+                break;
+
+                case 'module-two-columns' :
+                    get_template_part( 'inc/modules/module-two-columns' );
+                break;
+            endswitch;
+
+        endwhile;
+    endif;
+
+endwhile;
+?>
+</main>
+<?php get_footer(); ?>
