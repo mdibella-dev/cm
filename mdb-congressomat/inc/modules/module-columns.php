f9bafe00189efb86815f5862f7279ef49d3ce838
Marco Di Bella <dibella@rechtsdepesche.de>
2019-06-03 15:23:54 +0200

    module-columns

--- mdb-congressomat/inc/modules/module-columns.php
+++ mdb-congressomat/inc/modules/module-columns.php
@@ -8,36 +8,27 @@
 
 
 
-// Variablen setzen
-$module_id      = get_sub_field( 'module-id' );
-$module_classes = array( 'module',
-                         get_row_layout(),
-                         get_sub_field( 'module-additional-class' ) );
-
-
 // Content erstellen
 if( have_rows( 'module-content' ) ):
-
     // Modulinhalt zusammenstellen
     $module_content = '<div class="module-content"><div class="module-column-wrapper">';
     $column_count   = 0;
-
 
     // Spalteninhalte zusammenstellen
     while ( have_rows( 'module-content' ) ) :
         the_row();
 
         $column_count   += 1;
+        $column_id       = get_sub_field( 'module-column-id' );
         $column_classes  = array( 'module-column',
                                   get_sub_field( 'module-column-class' ),
                                   sprintf( 'module-column-%1$s', $column_count ) );
-        $module_content .= sprintf( '<div class="%1$s">%2$s</div>',
+        $module_content .= sprintf( '<div class="%1$s"%2$s>%3$s</div>',
                                     implode( ' ', $column_classes ),
+                                    ( !empty( $column_id ) )? sprintf( ' id="%1$s"', $column_id ) : '',
                                     get_sub_field( 'module-column-content' ) );
     endwhile;
     $module_content .= '</div></div>';
-
-    $module_classes[] = sprintf( 'module-columns-%1$s', $column_count );
 else :
     $module_content = '';
 endif;
@@ -45,6 +36,15 @@ endif;
 
 // Ausgabe
 if( !empty( $module_content) ) :
+    $module_column_class = array( 1 => 'module-one-column',
+                                  2 => 'module-two-column',
+                                  3 => 'module-three-column' );
+    $module_classes      = array( 'module',
+                                  get_row_layout(),
+                                  $module_column_class[ $column_count ],
+                                  get_sub_field( 'module-additional-class' ) );
+    $module_id           = get_sub_field( 'module-id' );
+
     echo sprintf( '<section class="%1$s"%2$s><div class="module-wrapper">%3$s</div></section>',
                   implode( ' ', $module_classes ),
                   ( !empty( $module_id ) )? sprintf( ' id="%1$s"', $module_id ) : '',
