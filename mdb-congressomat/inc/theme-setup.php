eaae1074a8d10acf51d230fee23737fd39bd43c3
Marco Di Bella <dibella@rechtsdepesche.de>
2019-09-11 09:10:03 +0200

    Theme-Support ergänzt

--- mdb-congressomat/inc/theme-setup.php
+++ mdb-congressomat/inc/theme-setup.php
@@ -23,6 +23,7 @@
     add_theme_support( 'post-thumbnails' );
     add_theme_support( 'align-wide' );
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
+    add_theme_support( 'responsive-embeds' );
 
     // Menüs registrieren
     register_nav_menu( 'primary', __( 'Primäre Navigation', TEXT_DOMAIN ) );
