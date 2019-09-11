0c2a824fd1e7e43caedf8599597f42fdc3b3c2ca
Marco Di Bella <dibella@rechtsdepesche.de>
2019-09-11 09:07:55 +0200

    Button versetzt

--- mdb-congressomat/inc/shortcodes/shortcode-teaser-list.php
+++ mdb-congressomat/inc/shortcodes/shortcode-teaser-list.php
@@ -116,9 +116,9 @@ function mdb_shortcode_teaser_list( $atts, $content = null )
 <div class="teaser-content">
 <h2><?php the_title(); ?></h2>
 <?php the_excerpt(); ?>
+</div>
 <div class="teaser-more">
 <a href="<?php the_permalink(); ?>" class="btn" title="<?php _e( 'Mehr erfahren', TEXT_DOMAIN ); ?>"><?php _e( 'Mehr erfahren', TEXT_DOMAIN ); ?></a>
-</div>
 </div>
 </article>
 <?php
