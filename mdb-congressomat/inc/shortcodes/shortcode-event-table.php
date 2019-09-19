933c15ed85c49a34b458498e2469b2725987cdba
Marco Di Bella <dibella@rechtsdepesche.de>
2019-09-19 15:23:19 +0200

    Event Table

--- mdb-congressomat/inc/shortcodes/shortcode-event-table.php
+++ mdb-congressomat/inc/shortcodes/shortcode-event-table.php
@@ -7,8 +7,7 @@
  * Folgende Parameter können verwendet werden:
  * @param   event    Die Identifikationsnummer des Events
  * @param   speaker  (optional) Die Identfikationsnummer eines Referenten; dient zur Filterung der Beiträge dieses Referenten.
- * @param   fieldset Eine kommaseparierte Liste mit Feldschlüsseln, mit denen die Auswahl sowie die Sortierung der Tabellenzeilen vorgenommen wird.
- *                   Folgende Werte sind derzeit möglich: VON, VONBIS, DATUM, TITEL, REFERENT, ORT
+ * @param   set      Die gewählte Set-Vorlage
  *
  * @author  Marco Di Bella <mdb@marcodibella.de>
  * @package mdb-congressomat
@@ -20,117 +19,59 @@ function mdb_shortcode_event_table( $atts, $content = null )
     extract( shortcode_atts( array(
                              'event'    => '',
                              'speaker'  => '',
-							 'fieldset' => 'VONBIS,TITEL,REFERENT,ORT'
+                             'set'      => '1',
                              ), $atts ) );
 
-    // Daten holen
-    $sessions = mdb_get_sessions_by_event( $event, $speaker );
 
-    // Ausgabe vorbereiten
-    $output = '';
+    $output  = '';
+    $rowset  = array();
+    $rowsets = array( '1' => 'session-date,session-time-range|session-title,session-subtitle|session-location',
+                      '2' => 'session-time-begin|session-title,session-subtitle,session-location|session-speaker',
+                      '3' => 'session-time-begin|session-title,session-subtitle|session-speaker' );
 
-    if( $sessions ) :
-        // Variablen setzen
-        $rows       = array();
-        $field_keys = explode( ',', strtoupper( str_replace(" ", "", $fieldset ) ) );
-        unset( $speaker );
+
+    if( ( 1 <= $set ) and ( $set <= sizeof( $rowsets) ) ) :
 
         /**
          * Schritt 1
-         * Alle Programmpunkte durchlaufen und entsprechende Tabellenzeilen generieren
+         * Rowset vorbereiten
          **/
 
-        foreach( $sessions as $session ) :
-            // Tabellenzellen zurücksetzen
-            unset( $cells );
+        $pass_1 = explode( '|', $rowsets[ $set ] );  // 1. Durchlauf
 
-            // Alle Feldschlüssel in der designierten Reihenfolge durchlaufen
-            foreach( $field_keys as $field_key ) :
-                switch( $field_key ) :
-                    case 'DATUM':
-                        $cells[ 'session-date' ] = get_field( 'programmpunkt-datum', $session->ID );
-                    break;
-
-                    case 'VON':
-                        $cells[ 'session-begin' ] = get_field( 'programmpunkt-von', $session->ID );
-                    break;
-
-                    case 'VONBIS':
-                        $cells[ 'session-time' ] = sprintf( '%1$s-%2$s',
-                                                            get_field( 'programmpunkt-von', $session->ID ),
-                                                            get_field( 'programmpunkt-bis', $session->ID ) );
-                    break;
-
-                    case 'TITEL':
-                        // Workaround #6
-                        // $title    = apply_filters( 'the_content', $session->post_title );
-                        $title    = $session->post_title;
-                        $subtitle = get_field( 'programmpunkt-untertitel', $session->ID );
-
-                        if( !empty( $title ) ) :
-                            $cells[ 'session-title' ] .= sprintf( '<span class="title">%1$s</span>', $title );
-
-                            if( !empty( $subtitle ) ) :
-                                $cells[ 'session-title' ] .= sprintf( '<span class="subtitle">%1$s</span>', $subtitle );
-                            endif;
-                        else :
-                            $cells[ 'session-title' ] = '';
-                        endif;
-                    break;
-
-                    case 'REFERENT':
-                        $speakers = get_field( 'programmpunkt-referenten', $session->ID );
-
-                        if( $speakers != NULL ) :
-                            unset( $speakers_list );
-
-                            foreach( $speakers as $speaker_id ) :
-                                $speaker         = mdb_get_speaker_info( $speaker_id );
-                                $speakers_list[] = sprintf( '<a href="%1$s" title="%2$s">%3$s</a>',
-                                                            $speaker[ 'permalink' ],
-                                                            sprintf( __( 'Mehr über %1$s erfahren', TEXT_DOMAIN ), $speaker[ 'title_name' ] ),
-                                                            get_the_post_thumbnail( $speaker[ 'id' ], 'full' ) );
-                            endforeach;
-
-                            $cells[ 'session-speaker' ] = implode( ' ', $speakers_list );
-                        else :
-                            $cells[ 'session-speaker' ] = '';
-                        endif;
-                    break;
-
-                    case 'ORT':
-                        $cells[ 'session-location' ] = mdb_get_location( get_field( 'programmpunkt-location', $session->ID ) );
-                    break;
-                endswitch;
-            endforeach;
-
-            // Alle Tabellenzellen zu einer Tabellenreihe zusammenbauen
-            $row_content = '';
-
-            foreach( $cells as $class => $cell ) :
-                $row_content .= sprintf( '<td class="%1$s">%2$s</td>', $class, $cell );
-            endforeach;
-
-            $rows[] = sprintf( '<tr>%1$s</tr>', $row_content );
+        foreach( $pass_1 as $pass_2 ) :
+            $rowset[] = explode( ',', $pass_2 );     // 2. Durchlauf
         endforeach;
 
 
         /**
-         * Schritt 3
-         * Ausgabe vorbereiten
+         * Schritt 2
+         * Die Daten aller Sessions des gewählten Events und Speakers holen
+         * und jede Session entlang des ermittelten Rowsets abarbeiten
          **/
 
-        $output .= '<div class="event-table">';
-        $output .= '<table>';
-        $output .= '<tbody>';
+        $sessions = mdb_get_sessions_by_event( $event, $speaker );
+        $rows     = '';
 
-        foreach( $rows as $row ) :
-            $output .= $row;
-        endforeach;
+        if( $sessions ) :
+            foreach( $sessions as $session ) :
+                $row = '';
 
-        $output .= '</tbody>';
-        $output .= '</table>';
-        $output .= '</div>';
+                foreach( $rowset as $pass_1 ) :
+                    $cell = '';
+
+                    foreach( $pass_1 as $pass_2 ) :
+                        $cell .= sprintf( '<div data-type="%1$s">%2$s</div>', $pass_2, mdb_get_session_data( $pass_2, $session ) );
+                    endforeach;
+
+                    $row .= sprintf( '<div class="cell">%1$s</div>', $cell );
+                endforeach;
+
+                $rows .= sprintf( '<div class="row">%1$s</div>', $row );
+            endforeach;
+
+            $output = sprintf( '<div class="event-table has-set-%1$s">%2$s</div>', $set, $rows );
+        endif;
     endif;
 
     // Ausgabe
@@ -138,3 +79,61 @@ function mdb_shortcode_event_table( $atts, $content = null )
 }
 
 add_shortcode( 'event-table', 'mdb_shortcode_event_table' );
+
+
+
+/**
+ * Hilfsfunktion, um die zweistufige Datenabfrage zu ermöglichen
+ */
+
+function mdb_get_session_data( $data_key, $session )
+{
+    $value = '';
+
+    switch( $data_key ) :
+        case 'session-date' :
+            $value = get_field( 'programmpunkt-datum', $session->ID );
+        break;
+
+        case 'session-time-begin' :
+            $value = get_field( 'programmpunkt-von', $session->ID );
+        break;
+
+        case 'session-time-range' :
+            $value = sprintf( '%1$s-%2$s', get_field( 'programmpunkt-von', $session->ID ),
+                                           get_field( 'programmpunkt-bis', $session->ID ) );
+        break;
+
+        case 'session-title' :
+            // $title    = apply_filters( 'the_content', $session->post_title );
+            $value = $session->post_title;
+        break;
+
+        case 'session-subtitle' :
+            $value = get_field( 'programmpunkt-untertitel', $session->ID );
+        break;
+
+        case 'session-location' :
+            $value = mdb_get_location( get_field( 'programmpunkt-location', $session->ID ) );
+        break;
+
+        case 'session-speaker' :
+            $speakers = get_field( 'programmpunkt-referenten', $session->ID );
+
+            if( $speakers != NULL ) :
+                unset( $speakers_list );
+
+                foreach( $speakers as $speaker_id ) :
+                    $speaker         = mdb_get_speaker_info( $speaker_id );
+                    $speakers_list[] = sprintf( '<a href="%1$s" title="%2$s">%3$s</a>',
+                                                $speaker[ 'permalink' ],
+                                                sprintf( __( 'Mehr über %1$s erfahren', TEXT_DOMAIN ), $speaker[ 'title_name' ] ),
+                                                get_the_post_thumbnail( $speaker[ 'id' ], 'full' ) );
+                endforeach;
+                $value = implode( ' ', $speakers_list );
+            endif;
+        break;
+    endswitch;
+
+    return $value;
+}
