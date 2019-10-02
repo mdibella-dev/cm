7a8da67fb7c340833ef2c6c0b7ec0a9cb2689154
Marco Di Bella <dibella@rechtsdepesche.de>
2019-10-02 16:20:22 +0200

    Blöcke und Tabellen

--- mdb-congressomat/assets/js/block-editor.js
+++ mdb-congressomat/assets/js/block-editor.js
@@ -1,32 +1,16 @@
 wp.domReady( () => {
-
-	wp.blocks.registerBlockStyle( 'core/table', {
-		name: 'fineline',
-		label: 'Fineline',
-		isDefault: false
-	} );
-
-	// Überschriften
-	wp.blocks.registerBlockStyle( 'core/heading', {
-		name: 'default',
-		label: 'Default',
-		isDefault: true
-	} );
-
-	wp.blocks.registerBlockStyle( 'core/heading', {
-		name: 'thin',
-		label: 'Thin',
-		isDefault: false
- 	} );
-
-	// Buttons
+	// Nichtgenutzte Standardstile entfernen
 	wp.blocks.unregisterBlockStyle( 'core/button', 'default' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'circular' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'outline' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'squared' );
 	wp.blocks.unregisterBlockStyle( 'core/button', 'shadow' );
 	wp.blocks.unregisterBlockStyle( 'core/button', '3d' );
+	wp.blocks.unregisterBlockStyle( 'core/table', 'regular' );
+	wp.blocks.unregisterBlockStyle( 'core/table', 'stripes' );
 
+
+	// Buttons
 	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'default',
 		label: 'Default',
@@ -36,6 +20,26 @@ wp.domReady( () => {
 	wp.blocks.registerBlockStyle( 'core/button', {
 		name: 'ghost',
 		label: 'Ghost',
+		isDefault: false
+	} );
+
+
+	// Table
+	wp.blocks.registerBlockStyle( 'core/table', {
+		name: 'default',
+		label: 'Standard',
+		isDefault: true
+	} );
+
+	wp.blocks.registerBlockStyle( 'core/table', {
+		name: 'default-with-header-column',
+		label: 'Standard mit Titelspalte',
+		isDefault: false
+	} );
+
+	wp.blocks.registerBlockStyle( 'core/table', {
+		name: 'default-with-header-row',
+		label: 'Standard mit Titelzeile',
 		isDefault: false
 	} );
 
