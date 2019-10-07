91ca5c309b2633c446d99f2e58d91c7c37fe0e1e
Marco Di Bella <dibella@rechtsdepesche.de>
2019-10-07 16:53:46 +0200

    Update theme-setup.php

--- mdb-congressomat/inc/theme-setup.php
+++ mdb-congressomat/inc/theme-setup.php
@@ -19,14 +19,26 @@
     // Lokalisation
 	load_theme_textdomain( TEXT_DOMAIN, PATH_THEME_TEMPLATE . '/lang' );
 
+
     // Theme-Support für diverse Features
-    add_theme_support( 'post-thumbnails' );
     add_theme_support( 'responsive-embeds' );
     add_theme_support( 'align-wide' );
     add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
 
+
+    // Thumbnails für Posts
+    add_theme_support( 'post-thumbnails' );
+    set_post_thumbnail_size( 240, 240, false );
+
+    if( ( get_option( 'thumbnail_size_w' ) != 240 ) ) :
+        update_option( 'thumbnail_size_w', 240 );
+        update_option( 'thumbnail_size_h', 240 );
+    endif;
+
+
     // Farbpalette für Blockeditor
     // Farben und Farbnamen entstammen https://coolors.co/
+    $gutenberg_palette = array();
     $palette = array(
                'Weiß, white, #fff',
                'Schwarz 10%, black-10, #e5e5e5',
@@ -39,17 +51,33 @@
                'Schwarz 80%, black-80, #333',
                'Schwarz 90%, black-90, #191919',
                'Schwarz, black, #000' );
-
-    $gutenberg_palette = array();
-
+    
     foreach( $palette as $color_set ) :
         $parts               = explode( ',',  $color_set );
         $gutenberg_palette[] = array( 'name'  => __( trim( $parts[0] ), TEXT_DOMAIN ),
                                       'slug'  => trim( $parts[1] ),
                                       'color' => trim( $parts[2] ) );
     endforeach;
-
     add_theme_support( 'editor-color-palette', $gutenberg_palette );
+
+
+    // Bildgrößen registrieren bzw. umdefinieren
+    // Angaben entsprechen den Gutenberg Media Queries
+    if( ( get_option( 'medium_size_w' ) != 782 ) ) :
+        update_option( 'medium_size_w', 782 );
+        update_option( 'medium_size_h', 9999 );
+    endif;
+
+    if( ( get_option( 'large_size_w' ) != 960 ) ) :
+        update_option( 'large_size_w', 960 );
+        update_option( 'large_size_h', 9999 );
+    endif;
+
+    add_image_size ( 'mobile', 480, 9999 );
+    add_image_size ( 'small', 600, 9999 );
+    add_image_size ( 'wide', 1280, 9999 );
+    add_image_size ( 'huge', 1440, 9999 );
+
 
     // Menüs registrieren
     register_nav_menu( 'primary', __( 'Primäre Navigation', TEXT_DOMAIN ) );
