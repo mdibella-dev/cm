8d5c65f4e7cb58331c25b39c451e3496f4ecfb47
rdg-dibella <dibella@rechtsdepesche.de>
2020-02-26 21:10:12 +0100

    Setup aufgeräumt

--- inc/setup.php
+++ inc/setup.php
@@ -8,117 +8,164 @@
 
 
 
-/**
- * Führt grundlegende Einstellungen für das Thema durch.
- *
- * @since   1.0.0
- **/
+if ( ! function_exists( 'congressomat_theme_setup' ) ) :
 
- function congressomat_theme_setup()
- {
-    // Laden der Lokalisation
-	load_theme_textdomain( 'congressomat', get_template_directory() . '/lang' );
+    /**
+     * Führt grundlegende Einstellungen für das Thema durch.
+     *
+     * @since   1.0.0
+     **/
 
+     function congressomat_theme_setup()
+     {
+        /**
+         * Unterstützung für Übersetzungen
+         **/
 
-    // Theme-Support für diverse Features
-    add_theme_support( 'responsive-embeds' );
-    add_theme_support( 'align-wide' );
-    add_theme_support( 'html5', array( 'comment-list', 'comment-form', 'search-form', 'gallery', 'caption' ) );
-
-
-    // Thumbnails für Posts
-    add_theme_support( 'post-thumbnails' );
-    set_post_thumbnail_size( 240, 240, false );
-
-    if( ( get_option( 'thumbnail_size_w' ) != 240 ) ) :
-        update_option( 'thumbnail_size_w', 240 );
-        update_option( 'thumbnail_size_h', 240 );
-    endif;
+	    load_theme_textdomain( 'congressomat', get_template_directory() . '/lang' );
 
 
-    // Farbpalette für Blockeditor
-    // Farben und Farbnamen entstammen https://coolors.co/
-    $gutenberg_palette = array();
-    $palette = array(
-        'Weiß, white, #fff',
-        'Schwarz 5%, black-05, #f0f0f0',
-        'Schwarz 10%, black-10, #e5e5e5',
-        'Schwarz 20%, black-20, #ccc',
-        'Schwarz 30%, black-30, #b2b2b2',
-        'Schwarz 40%, black-40, #999',
-        'Schwarz 50%, black-50, #7f7f7f',
-        'Schwarz 60%, black-60, #666',
-        'Schwarz 70%, black-70, #4c4c4c',
-        'Schwarz 80%, black-80, #333',
-        'Schwarz 90%, black-90, #191919',
-        'Schwarz, black, #000'
-    );
+        /**
+         * HTML5-Konformität für bestimmte WordPress-Core-Elementen
+         **/
 
-    foreach( $palette as $color_set ) :
-        $parts               = explode( ',',  $color_set );
-        $gutenberg_palette[] = array(
-            'name'  => __( trim( $parts[0] ), 'congressomat' ),
-            'slug'  => trim( $parts[1] ),
-            'color' => trim( $parts[2] )
+        add_theme_support( 'html5', array(
+            'comment-list',
+            'comment-form',
+            'search-form',
+            'gallery',
+            'caption',
+            'script',
+            'style',
+        ) );
+
+
+        /**
+         * Responsive Einbettung von Embeds ermöglichen
+         **/
+
+        add_theme_support( 'responsive-embeds' );
+
+
+        /**
+         * Thumbnails
+         **/
+
+        // Thumbnails auch für Posts
+        add_theme_support( 'post-thumbnails' );
+
+        // Thumbnailgröße
+        set_post_thumbnail_size( 240, 240, false );
+
+        if( ( get_option( 'thumbnail_size_w' ) != 240 ) ) :
+            update_option( 'thumbnail_size_w', 240 );
+            update_option( 'thumbnail_size_h', 240 );
+        endif;
+
+
+        /**
+         * Blockeditor (Gutenberg)
+         **/
+
+        // Unterstützung für "align full"/"align wide"
+         add_theme_support( 'align-wide' );
+
+        // Farbpalette
+        // Farben und Farbnamen entstammen https://coolors.co/
+        $blockeditor_palette  = array();
+        $congressomat_palette = array(
+            'Weiß, white, #fff',
+            'Schwarz 10%, black-10, #e5e5e5',
+            'Schwarz 20%, black-20, #ccc',
+            'Schwarz 30%, black-30, #b2b2b2',
+            'Schwarz 40%, black-40, #999',
+            'Schwarz 50%, black-50, #7f7f7f',
+            'Schwarz 60%, black-60, #666',
+            'Schwarz 70%, black-70, #4c4c4c',
+            'Schwarz 80%, black-80, #333',
+            'Schwarz 90%, black-90, #191919',
+            'Schwarz, black, #000'
         );
-    endforeach;
-    add_theme_support( 'editor-color-palette', $gutenberg_palette );
+
+        foreach( $congressomat_palette as $set ) :
+            $part                  = explode( ',', $set );
+            $blockeditor_palette[] = array(
+                'name'  => __( trim( $part[0] ), 'congressomat' ),
+                'slug'  => trim( $part[1] ),
+                'color' => trim( $part[2] )
+            );
+        endforeach;
+        add_theme_support( 'editor-color-palette', $blockeditor_palette );
 
 
-    // Registrierung/Neudefinierung der Bildgrößen
-    // Angaben entsprechen den Gutenberg Media Queries
-    if( ( get_option( 'medium_size_w' ) != 782 ) ) :
-        update_option( 'medium_size_w', 782 );
-        update_option( 'medium_size_h', 9999 );
-    endif;
+        /**
+         * Mediengrößen
+         *
+         * Größenangaben entsprechen den Gutenberg Media Queries
+        **/
 
-    if( ( get_option( 'large_size_w' ) != 960 ) ) :
-        update_option( 'large_size_w', 960 );
-        update_option( 'large_size_h', 9999 );
-    endif;
+        if( ( get_option( 'medium_size_w' ) != 782 ) ) :
+            update_option( 'medium_size_w', 782 );
+            update_option( 'medium_size_h', 9999 );
+        endif;
 
-    add_image_size ( 'mobile', 480, 9999 );
-    add_image_size ( 'small', 600, 9999 );
-    add_image_size ( 'wide', 1280, 9999 );
-    add_image_size ( 'huge', 1440, 9999 );
-    remove_image_size( 'medium_large' );
+        if( ( get_option( 'large_size_w' ) != 960 ) ) :
+            update_option( 'large_size_w', 960 );
+            update_option( 'large_size_h', 9999 );
+        endif;
+
+        add_image_size ( 'mobile', 480, 9999 );
+        add_image_size ( 'small', 600, 9999 );
+        add_image_size ( 'wide', 1280, 9999 );
+        add_image_size ( 'huge', 1440, 9999 );
+        remove_image_size( 'medium_large' );
 
 
-    // Registrierung der Hauptnavigation
-    register_nav_menu( 'primary', __( 'Primäre Navigation', 'congressomat' ) );
+        /**
+         * Navigation
+         **/
+
+        register_nav_menu( 'primary', __( 'Primäre Navigation', 'congressomat' ) );
 
 
-    // Registrierung der Widget-Areas im Bereich der Fußzeile
-    register_sidebar( array(
-    	'name'			=> __( 'Footer #1', 'congressomat' ),
-    	'id'			=> 'footer-one',
-    	'description'	=> __( 'Die hier abgelegten Widgets erscheinen im Bereich 1 in der Fußzeile.', 'congressomat' ),
-    	'before_widget'	=> '<div class="widget %2$s clr">',
-    	'after_widget'	=> '</div>',
-    	'before_title'	=> '<h6 class="widget-title">',
-    	'after_title'	=> '</h6>',
-    ) );
+        /**
+         * Widgets
+         **/
 
-    register_sidebar( array(
-    	'name'			=> __( 'Footer #2', 'congressomat' ),
-    	'id'			=> 'footer-two',
-    	'description'	=> __( 'Die hier abgelegten Widgets erscheinen im Bereich 2 in der Fußzeile.', 'congressomat' ),
-    	'before_widget'	=> '<div class="widget %2$s clr">',
-    	'after_widget'	=> '</div>',
-    	'before_title'	=> '<h6 class="widget-title">',
-    	'after_title'	=> '</h6>',
-    ) );
+        register_sidebar( array(
+    	    'name'			=> __( 'Footer #1', 'congressomat' ),
+    	    'id'			=> 'footer-one',
+    	    'description'	=> __( 'Die hier abgelegten Widgets erscheinen im Bereich 1 in der Fußzeile.', 'congressomat' ),
+    	    'before_widget'	=> '<div class="widget %2$s clr">',
+    	    'after_widget'	=> '</div>',
+    	    'before_title'	=> '<h6 class="widget-title">',
+    	    'after_title'	=> '</h6>',
+        ) );
 
-    register_sidebar( array(
-    	'name'			=> __( 'Footer #3', 'congressomat' ),
-    	'id'			=> 'footer-three',
-    	'description'	=> __( 'Die hier abgelegten Widgets erscheinen im Bereich 3 in der Fußzeile.', 'congressomat' ),
-    	'before_widget'	=> '<div class="widget %2$s clr">',
-    	'after_widget'	=> '</div>',
-    	'before_title'	=> '<h6 class="widget-title">',
-    	'after_title'	=> '</h6>',
-    ) );
-}
+        register_sidebar( array(
+        	'name'			=> __( 'Footer #2', 'congressomat' ),
+        	'id'			=> 'footer-two',
+        	'description'	=> __( 'Die hier abgelegten Widgets erscheinen im Bereich 2 in der Fußzeile.', 'congressomat' ),
+        	'before_widget'	=> '<div class="widget %2$s clr">',
+        	'after_widget'	=> '</div>',
+        	'before_title'	=> '<h6 class="widget-title">',
+        	'after_title'	=> '</h6>',
+        ) );
+
+        register_sidebar( array(
+        	'name'			=> __( 'Footer #3', 'congressomat' ),
+        	'id'			=> 'footer-three',
+        	'description'	=> __( 'Die hier abgelegten Widgets erscheinen im Bereich 3 in der Fußzeile.', 'congressomat' ),
+        	'before_widget'	=> '<div class="widget %2$s clr">',
+        	'after_widget'	=> '</div>',
+        	'before_title'	=> '<h6 class="widget-title">',
+        	'after_title'	=> '</h6>',
+        ) );
+    }
+
+    add_action( 'after_setup_theme', 'congressomat_theme_setup' );
+
+endif;
 
 
 
@@ -130,21 +177,35 @@
 
 function congressomat_enqueue_scripts()
 {
-    // Verschieben der von WordPress gelieferten jQuery in den Footer
+    /**
+     * Skripte registrieren und in den Footer laden
+     **/
+
+    // Verschieben der von WordPress gelieferten jQuery
     wp_deregister_script( 'jquery' );
     wp_register_script( 'jquery', '/wp-includes/js/jquery/jquery.js', false, false, true );
     wp_enqueue_script( 'jquery' );
 
-
-    // Registrieren und Laden eigener Skripte
+    // Skript von Congressomat
     wp_register_script( 'congressomat', get_template_directory_uri() . '/assets/js/frontend.js', array( 'jquery' ), false, true );
     wp_enqueue_script( 'congressomat' );
 
 
-    // Integration von FontAwesome5
+    /**
+     * Integration von FontAwesome5
+     **/
+
 	wp_enqueue_style( 'fontawesome', get_template_directory_uri() . '/assets/fa5/css/fontawesome-all.min.css' );
 
 
-	// Laden des eigenen Stylesheets
-	wp_enqueue_style( 'congressomat', get_template_directory_uri() . '/assets/css/frontend.min.css' );
+    /**
+     * Registrieren und Laden des Standard-Stylesheets von Congressomat
+     *
+     * style.css im Hauptverzeichnis dient nur zur Theme-Identifikation und -Versionierung
+     * Die (komprimierten) Stilangaben befinden sich tatsächlich in frontend(.min).css
+     **/
+
+    wp_enqueue_style( 'congressomat', get_template_directory_uri() . '/assets/css/frontend.min.css' );
 }
+
+add_action( 'wp_enqueue_scripts', 'congressomat_enqueue_scripts', 9990 );
