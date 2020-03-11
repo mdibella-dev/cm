d6edf57b610429bbedf9c264f31112c3fdd7574a
rdg-dibella <dibella@rechtsdepesche.de>
2020-03-11 22:04:53 +0100

    Download hinzugefügt

---
+++ download-monitor/content-download-link.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Custom Template für einfache Download-Links
+ *
+ * @author Marco Di Bella <mdb@marcodibella.de>
+ * @package congressomat-winterakademie
+ * @uses Plugin download-monitor
+ */
+
+
+
+/* Aussteigen, wenn direkt aufgerufen */
+
+if( !defined( 'ABSPATH' ) ) :
+    exit;
+endif;
+
+?>
+<div class="download">
+    <i class="fas fa-download"></i>
+    <div>
+        <a href="<?php $dlm_download->the_download_link(); ?>"
+            rel="nofollow"
+            title="<?php _e( 'Datei herunterladen', 'congressomat' ); ?>">
+            <?php $dlm_download->the_title(); ?>
+        </a>
+    </div>
+</div>
