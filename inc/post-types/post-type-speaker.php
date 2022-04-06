49c81604f689b6bc939846bc6d1c39b8d440cdff
Marco Di Bella <dibella@rechtsdepesche.de>
2022-04-06 10:02:08 +0200

    Sortierfunktion entfernt (defunct)

--- inc/post-types/post-type-speaker.php
+++ inc/post-types/post-type-speaker.php
@@ -23,9 +23,7 @@ function cm_post_type_speaker__manage_posts_columns( $default )
 {
     $columns['cb']               = $default['cb'];
     $columns['image']            = __( 'Bild', 'mdb' );
-    $columns['name']             = __( 'Referent', 'mdb' );
-    $columns['forename']         = __( 'Vorname', 'mdb' );
-    $columns['lastname']         = __( 'Nachname', 'mdb' );
+    $columns['title']            = __( 'Referent/in', 'mdb' );
     $columns['shortdescription'] = __( 'Kurzbeschreibung', 'mdb' );
 
     return $columns;
@@ -59,14 +57,6 @@ function cm_post_type_speaker__manage_posts_custom_column( $column_name, $post_i
             endif;
         break;
 
-        case 'forename':
-            echo get_field( 'referent-vorname', $post_id );
-        break;
-
-        case 'lastname':
-            echo get_field( 'referent-nachname', $post_id );
-        break;
-
         case 'shortdescription':
             echo trim( implode( ' ', array(
                 get_field( 'referent-titel', $post_id ),
@@ -85,53 +75,3 @@ function cm_post_type_speaker__manage_posts_custom_column( $column_name, $post_i
 }
 
 add_action( 'manage_speaker_posts_custom_column', 'cm_post_type_speaker__manage_posts_custom_column', 9999, 2 );
-
-
-
-/**
- * Registriert sortierfähige Spalten (durch Zuordnung entsprechender orderby-Parameter).
- *
- * @since  2.5.0
- * @param  array  $columns   Die Spalten.
- * @return $array            Ein assoziatives Array.
- */
-
-function cm_post_type_speaker__manage_sortable_columns( $columns )
-{
-    $columns['forename'] = 'forename';
-    $columns['lastname'] = 'lastname';
-    return $columns;
-}
-
-add_filter( 'manage_edit-speaker_sortable_columns', 'cm_post_type_speaker__manage_sortable_columns' );
-
-
-
-/**
- * Erzeugt eine sortierte Ausgabe.
- *
- * @since 2.5.0
- * @param WP_Query $query   Ein Datenobjekt der zuletzt gemachten Abfrage.
- */
-
-function cm_post_type_speaker__pre_get_posts( $query )
-{
-    if( $query->is_main_query() and is_admin() ) :
-
-        $orderby = $query->get( 'orderby' );
-
-        switch( $orderby ) :
-
-            case 'forename':
-                $query->set( 'orderby', 'forename' );
-            break;
-
-            case 'lastname':
-                $query->set( 'orderby', 'lastname' );
-            break;
-
-        endswitch;
-    endif;
-}
-
-add_action( 'pre_get_posts', 'cm_post_type_speaker__pre_get_posts', 1 );
