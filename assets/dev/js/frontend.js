34c36b6d8af35a1ba3fa9e0f396ed78f76648140
Marco Di Bella <dibella@rechtsdepesche.de>
2022-11-18 14:51:38 +0100

    Revert "Backport live-panel support"
    This reverts commit e11030a8b187fb9102438597db7f80092dbaf520.

--- assets/dev/js/frontend.js
+++ assets/dev/js/frontend.js
@@ -25,28 +25,6 @@ jQuery(function($) {
     } );
 
 
-    // Live-Panels
-    $( '.live-panel-list > ul > .live-panel > .live-panel-caption' ).click(function() {
-
-        var this_panel = $( this ).parent();
-
-        if( ! this_panel.hasClass( 'open' ) ) {
-
-            var open_panel = $( this ).parents( '.live-panel-list > ul' ).find('.open');
-
-            open_panel.removeClass( 'open' );
-            this_panel.addClass( 'open' );
-            this_panel.children( '.live-panel-content' ).slideToggle( 'medium' );
-            open_panel.children( '.live-panel-content' ).slideToggle( 'medium' );
-        } else {
-            this_panel.removeClass( 'open' );
-            this_panel.children( '.live-panel-content' ).slideToggle( 'medium' );
-        }
-
-        e.stopPropagation();
-    } );
-
-
     // Korrektur von wpautop
     $( 'p:empty' ).remove();
 
