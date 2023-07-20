b7b90ea7a956bc74a448be7d8b2d67ba325fb74d
Marco Di Bella <mdb@marcodibella.de>
2023-07-20 11:48:42 +0200

    Change array notation

--- includes/setup.php
+++ includes/setup.php
@@ -28,7 +28,7 @@ if( ! function_exists( 'cm_theme_setup' ) ) :
 
 
         // Enables HTML5-compliant handling of various WordPress core elements.
-        add_theme_support( 'html5', array(
+        add_theme_support( 'html5', [
             'comment-list',
             'comment-form',
             'search-form',
@@ -36,7 +36,7 @@ if( ! function_exists( 'cm_theme_setup' ) ) :
             'caption',
             'script',
             'style',
-        ) );
+        ] );
 
 
         // Enables responsive embedding of media embeds.
@@ -44,68 +44,68 @@ if( ! function_exists( 'cm_theme_setup' ) ) :
 
 
         // Sets the block-editor palette.
-        add_theme_support( 'editor-color-palette', array(
-            array(
+        add_theme_support( 'editor-color-palette', [
+            [
                 'name'  => __( 'Weiß', 'cm' ),
                 'slug'  => 'white',
                 'color' => '#ffffff',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 5%', 'cm' ),
                 'slug'  => 'black-05',
                 'color' => '#fafafa',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 10%', 'cm' ),
                 'slug'  => 'black-10',
                 'color' => '#e5e5e5',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 20%', 'cm' ),
                 'slug'  => 'black-20',
                 'color' => '#cccccc',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 30%', 'cm' ),
                 'slug'  => 'black-30',
                 'color' => '#b2b2b2',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 40%', 'cm' ),
                 'slug'  => 'black-40',
                 'color' => '#999999',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 50%', 'cm' ),
                 'slug'  => 'black-50',
                 'color' => '#7f7f7f',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 60%', 'cm' ),
                 'slug'  => 'black-60',
                 'color' => '#666666',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 70%', 'cm' ),
                 'slug'  => 'black-70',
                 'color' => '#4c4c4c',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 80%', 'cm' ),
                 'slug'  => 'black-80',
                 'color' => '#333333',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz 90%', 'cm' ),
                 'slug'  => 'black-90',
                 'color' => '#191919',
-            ),
-            array(
+            ],
+            [
                 'name'  => __( 'Schwarz', 'cm' ),
                 'slug'  => 'black',
                 'color' => '#000000',
-            ),
-        ) );
+            ],
+        ] );
 
 
         // Enables 'wide' support for the block editor (Gutenberg).
@@ -137,7 +137,7 @@ if( ! function_exists( 'cm_theme_setup' ) ) :
 
 
         // Registers the widget areas.
-        register_sidebar( array(
+        register_sidebar( [
             'name'          => __( 'Footer #1', 'cm' ),
             'id'            => 'footer-one',
             'description'   => __( 'Die hier abgelegten Widgets erscheinen im Bereich 1 in der Fußzeile.', 'cm' ),
@@ -145,9 +145,9 @@ if( ! function_exists( 'cm_theme_setup' ) ) :
             'after_widget'  => '</div>',
             'before_title'  => '<h6 class="widget-title">',
             'after_title'   => '</h6>',
-        ) );
+        ] );
 
-        register_sidebar( array(
+        register_sidebar( [
             'name'          => __( 'Footer #2', 'cm' ),
             'id'            => 'footer-two',
             'description'   => __( 'Die hier abgelegten Widgets erscheinen im Bereich 2 in der Fußzeile.', 'cm' ),
@@ -155,9 +155,9 @@ if( ! function_exists( 'cm_theme_setup' ) ) :
             'after_widget'  => '</div>',
             'before_title'  => '<h6 class="widget-title">',
             'after_title'   => '</h6>',
-        ) );
+        ] );
 
-        register_sidebar( array(
+        register_sidebar( [
             'name'          => __( 'Footer #3', 'cm' ),
             'id'            => 'footer-three',
             'description'   => __( 'Die hier abgelegten Widgets erscheinen im Bereich 3 in der Fußzeile.', 'cm' ),
@@ -165,7 +165,7 @@ if( ! function_exists( 'cm_theme_setup' ) ) :
             'after_widget'  => '</div>',
             'before_title'  => '<h6 class="widget-title">',
             'after_title'   => '</h6>',
-        ) );
+        ] );
     }
 
     add_action( 'after_setup_theme', 'cm_theme_setup' );
@@ -185,7 +185,9 @@ function cm_enqueue_scripts()
     wp_enqueue_script(
         'cm-script',
         get_template_directory_uri() . '/assets/build/js/frontend.min.js',
-        array( 'jquery' ),
+        [
+            'jquery'
+        ],
         THEME_VERSION,
         true
     );
